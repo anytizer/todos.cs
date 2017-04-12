ea1fa259fb6e92e7fd069a82ec6cbfb62e8b7d74
bimal <gist@bimal.org.np>
2017-04-11 20:42:04 -0600

    renaming to `awesome`.

--- sql/database.sql
+++ sql/database.sql
@@ -1,81 +1,44 @@
-SHOW CREATE TABLE todo_statuses;
-CREATE TABLE `todo_statuses` (
-  `status_id` VARCHAR(255) NOT NULL DEFAULT '' COMMENT 'GUID',
-  `status_code` VARCHAR(255) NOT NULL DEFAULT '' COMMENT 'Status Code',
-  `status_name` VARCHAR(255) NOT NULL DEFAULT '' COMMENT 'Status Name',
-  `status_priority` INT(10) NOT NULL DEFAULT '0' COMMENT 'Sorting Priority',
-  PRIMARY KEY (`status_id`)
+-- DROP DATABASE IF EXISTS awesome_todos;
+-- CREATE DATABASE awesome_todos CHARACTER SET utf8 COLLATE utf8_general_ci;
+-- USE awesome_todos;
+
+-- SHOW CREATE TABLE todo_statuses;
+DROP TABLE IF EXISTS todo_statuses;
+CREATE TABLE todo_statuses (
+  status_id VARCHAR(255) NOT NULL DEFAULT '' COMMENT 'GUID',
+  status_code VARCHAR(255) NOT NULL DEFAULT '' COMMENT 'Status Code',
+  status_name VARCHAR(255) NOT NULL DEFAULT '' COMMENT 'Status Name',
+  status_priority INT(10) NOT NULL DEFAULT '0' COMMENT 'Sorting Priority',
+  PRIMARY KEY (status_id)
 );
 
-SHOW CREATE TABLE todo_projects;
-CREATE TABLE `todo_projects` (
-  `project_id` VARCHAR(255) NOT NULL DEFAULT '' COMMENT 'GUID',
-  `project_name` VARCHAR(255) NOT NULL DEFAULT '',
-  `is_active` ENUM('N','Y') DEFAULT 'N',
-  PRIMARY KEY (`project_id`)
+-- SHOW CREATE TABLE todo_projects;
+DROP TABLE IF EXISTS todo_projects;
+CREATE TABLE todo_projects (
+  project_id VARCHAR(255) NOT NULL DEFAULT '' COMMENT 'GUID',
+  project_name VARCHAR(255) NOT NULL DEFAULT '',
+  is_active ENUM('N','Y') DEFAULT 'N',
+  PRIMARY KEY (project_id)
 );
 
-SHOW CREATE TABLE todo_todos;
-CREATE TABLE `todo_todos` (
-  `todo_id` VARCHAR(255) NOT NULL DEFAULT '',
-  `project_id` VARCHAR(255) NOT NULL DEFAULT '',
-  `status_id` VARCHAR(255) NOT NULL DEFAULT '',
-  `issue_number` DATETIME NOT NULL,
-  `todo_text` TEXT NOT NULL,
-  `added_on` DATETIME NOT NULL,
-  PRIMARY KEY (`todo_id`),
-  KEY `project_id` (`project_id`),
-  KEY `status_id` (`status_id`),
-  CONSTRAINT `todo_todo_ibfk_1` FOREIGN KEY (`project_id`) REFERENCES `todo_projects` (`project_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `todo_todo_ibfk_2` FOREIGN KEY (`status_id`) REFERENCES `todo_statuses` (`status_id`) ON DELETE CASCADE ON UPDATE CASCADE
+-- SHOW CREATE TABLE todo_todos;
+DROP TABLE IF EXISTS todo_todos;
+CREATE TABLE todo_todos (
+  todo_id VARCHAR(255) NOT NULL DEFAULT '' COMMENT 'GUID',
+  project_id VARCHAR(255) NOT NULL DEFAULT '',
+  status_id VARCHAR(255) NOT NULL DEFAULT '',
+  issue_number VARCHAR(255) NOT NULL DEFAULT '',
+  todo_text TEXT NOT NULL,
+  added_on DATETIME NOT NULL,
+  is_active ENUM('N','Y') NOT NULL DEFAULT 'N',
+  PRIMARY KEY (todo_id),
+  KEY project_id (project_id),
+  KEY status_id (status_id),
+  CONSTRAINT todo_todos_ibfk_1 FOREIGN KEY (project_id) REFERENCES todo_projects (project_id),
+  CONSTRAINT todo_todos_ibfk_2 FOREIGN KEY (status_id) REFERENCES todo_statuses (status_id)
 );
 
-SHOW CREATE TABLE todo_todos;
-CREATE TABLE `todo_todos` (
-  `todo_id` VARCHAR(255) NOT NULL DEFAULT '' COMMENT 'GUID',
-  `project_id` VARCHAR(255) NOT NULL DEFAULT '',
-  `status_id` VARCHAR(255) NOT NULL DEFAULT '',
-  `issue_number` VARCHAR(255) NOT NULL DEFAULT '',
-  `todo_text` VARCHAR(255) NOT NULL DEFAULT '',
-  `added_on` DATETIME NOT NULL,
-  `is_active` ENUM('N','Y') NOT NULL DEFAULT 'N',
-  PRIMARY KEY (`todo_id`),
-  KEY `project_id` (`project_id`),
-  KEY `status_id` (`status_id`),
-  CONSTRAINT `todo_todos_ibfk_1` FOREIGN KEY (`project_id`) REFERENCES `todo_projects` (`project_id`),
-  CONSTRAINT `todo_todos_ibfk_2` FOREIGN KEY (`status_id`) REFERENCES `todo_statuses` (`status_id`)
-);
-
-
-SHOW CREATE TABLE todo_statuses;
-CREATE TABLE `todo_statuses` (
-  `status_id` VARCHAR(255) NOT NULL DEFAULT '',
-  `status_name` VARCHAR(255) NOT NULL DEFAULT '',
-  `status_code` VARCHAR(255) DEFAULT '',
-  PRIMARY KEY (`status_id`)
-);
-
-SHOW CREATE TABLE todo_projects;
-CREATE TABLE `todo_projects` (
-  `project_id` VARCHAR(255) NOT NULL DEFAULT '',
-  `project_name` VARCHAR(255) NOT NULL DEFAULT '',
-  PRIMARY KEY (`project_id`)
-);
-
-SHOW CREATE TABLE todo_todo;
-CREATE TABLE `todo_todo` (
-  `todo_id` VARCHAR(255) NOT NULL DEFAULT '',
-  `project_id` VARCHAR(255) NOT NULL DEFAULT '',
-  `status_id` VARCHAR(255) NOT NULL DEFAULT '',
-  `added_on` DATETIME NOT NULL,
-  `todo_text` TEXT NOT NULL,
-  PRIMARY KEY (`todo_id`),
-  KEY `project_id` (`project_id`),
-  KEY `status_id` (`status_id`),
-  CONSTRAINT `todo_todo_ibfk_1` FOREIGN KEY (`project_id`) REFERENCES `todo_projects` (`project_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `todo_todo_ibfk_2` FOREIGN KEY (`status_id`) REFERENCES `todo_statuses` (`status_id`) ON DELETE CASCADE ON UPDATE CASCADE
-);
-
+-- SHOW CREATE VIEW v_todos;
 DROP VIEW IF EXISTS v_todos;
 CREATE VIEW v_todos AS
 SELECT
@@ -89,12 +52,14 @@ INNER JOIN todo_statuses s ON s.status_id = t.status_id
 WHERE
 	t.is_active='Y'
 ;
+-- SELECT * FROM v_todos;
 
-SHOW CREATE TABLE todo_projects_statuses;
-CREATE TABLE `todo_projects_statuses` (
-  `history_id` VARCHAR(255) NOT NULL DEFAULT '',
-  `project_id` VARCHAR(255) NOT NULL DEFAULT '',
-  `status_id` VARCHAR(255) NOT NULL DEFAULT '',
-  `status_on` DATETIME NOT NULL,
-  PRIMARY KEY (`history_id`)
+-- SHOW CREATE TABLE todo_projects_statuses;
+DROP TABLE IF EXISTS todo_projects_statuses;
+CREATE TABLE todo_projects_statuses (
+  history_id VARCHAR(255) NOT NULL DEFAULT '',
+  project_id VARCHAR(255) NOT NULL DEFAULT '',
+  status_id VARCHAR(255) NOT NULL DEFAULT '',
+  status_on DATETIME NOT NULL,
+  PRIMARY KEY (history_id)
 );
