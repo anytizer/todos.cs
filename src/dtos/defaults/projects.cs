ce456891b02ebb7739d78f970c4f071ef13b1e53
Bimal <bimal@mynmax.com>
2017-04-12 10:57:55 -0600

    relocation

---
+++ src/dtos/defaults/projects.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dtos.defaults
+{
+    public class projects
+    {
+        public static Guid ProjectID = new Guid("640BC432-CDFF-4B7D-8D04-C418CD989AA3");
+    }
+}
---
+++ src/dtos/defaults/statuses.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dtos.defaults
+{
+    public class statuses
+    {
+        // SELECT CONCAT('public static Guid ', status_code, '= new Guid("', status_id, '");') FROM todo_statuses;
+        public static Guid LOWPRIORITY = new Guid("E827C910-5235-4C87-9F13-DAF960682D53");
+        public static Guid NEW = new Guid("E827C910-5235-4C87-9F13-DAF960682D54");
+        public static Guid DOING = new Guid("E827C910-5235-4C87-9F13-DAF960682D55");
+        public static Guid DONE = new Guid("E827C910-5235-4C87-9F13-DAF960682D56");
+        public static Guid NOTNEEDED = new Guid("E827C910-5235-4C87-9F13-DAF960682D57");
+        public static Guid CANNOTBEDONE = new Guid("E827C910-5235-4C87-9F13-DAF960682D58");
+        public static Guid DELETED = new Guid("E827C910-5235-4C87-9F13-DAF960682D59");
+    }
+}
