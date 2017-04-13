5c7d271854f78e7df6fd144c45693e85bd96bf3d
bimal <gist@bimal.org.np>
2017-04-12 21:25:49 -0600

    api to get all statuses.

--- src/database/statuses.cs
+++ src/database/statuses.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using database.mysql;
+using dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace database
 {
-    public class statuses
+    public partial class api: BaseAPI
     {
         /**
          * Status repository
@@ -10,6 +14,22 @@ namespace database
         public Guid delete_status()
         {
             return dtos.defaults.statuses.DELETED;
+        }
+
+        public List<NameValueDTO> all_statuses()
+        {
+            List<NameValueDTO> l = new List<NameValueDTO>();
+            foreach(todo_statuses status in this.te.todo_statuses.Where(x=>x.in_menu =="Y"))
+            {
+                NameValueDTO st = new NameValueDTO();
+                st.id = new Guid(status.status_id);
+                st.name = status.status_name;
+                st.value = status.status_code;
+
+                l.Add(st);
+            }
+
+            return l;
         }
     }
 }
