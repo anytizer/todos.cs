b41a68abf615e3d2761b5abc1fc0d79469d61eed
bimal <gist@bimal.org.np>
2017-04-15 13:46:40 -0600

    dump database structure.

--- sql/backup.bat
+++ sql/backup.bat
@@ -1,3 +1,4 @@
 @echo off
 mysqldump -uroot awesome_todos todo_statuses > status.dmp
 mysqldump -uroot --routines awesome_todos > full.dmp
+mysqldump -uroot --routines --no-data awesome_todos > structures.dmp
